ded15ca3169aae57d7f7b0a8d25beb534738d4b5
Detective954 <ashu954688@gmail.com>
2021-05-26 23:28:31 +0530

    functions

--- Assignment1.cpp
+++ Assignment1.cpp
@@ -10,12 +10,25 @@ int addSiteToHistory(string url){
 }
 
 bool isBrowsingHistoryEmpty(){
+		if(s1.empty() )
+	{
+		return true;
+	}
+	else{
+		return false;
+	}
 
 }
 string mostRecentlyVisitedSite(){
 	return s1.top();
 }
 void goBackInTime(int n){
+	if(!isBrowsingHistoryEmpty()){
+		while(n--){
+			s1.pop();
+		}
+	}
+	
 
 }
 string printBrowsingHistory(){
@@ -25,5 +38,65 @@ string printBrowsingHistory(){
 }
 int main(){
 
+	int ch;
+	while(1)
+	{
+		cout<<"1.Visit url"<<endl;
+		cout<<"2.Empty or not"<<endl;
+		cout<<"3.Recently Visited"<<endl;
+		cout<<"4.Go back in Time"<<endl;
+		cout<<"5.Browsing History"<<endl;
+		cout<<"6.Exit"<<endl;
+		
+		cout<<"Enter ur choice:";
+		cin>>ch;
+	
+	switch(ch)
+	{
+	case 1:{
+	    string s;
+	       cout<<"Enter website name to visit:"<<endl;
+	       cin>>s;
+	       addSiteToHistory(s);
+	       break;}
+		   
+	             
+	case 2: {
+	
+	         bool b=isBrowsingHistoryEmpty();
+	         cout<<b<<endl;
+	         break;
+	        }	
+  
+	case 3: {
+	         cout<<mostRecentlyVisitedSite();
+	         break;
+	     }
+	case 4: {
+	      int n;
+	     cout<<"Enter n to delete most recent history:";
+	     cin>>n;
+	     goBackInTime(n);
+	     cout<<"Deleted";
+	     break;
+	 }
+	
+	case 5:{
+		     while(!s1.empty())
+		     {
+		     	cout<<printBrowsingHistory()<<endl;
+		     	s1.pop();
+			 }
+			 break;
+           }
+	
 
+	case 6:exit(0);
+	default:{
+		
+		cout<<"Invalid Input/n/n";
+	}
+}
+}
+	
 }
